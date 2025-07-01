893464fa8ef588a06ca06c3a98e3a519ca05606c
Furkan Kümüş <105515121+Furkan-Kumus@users.noreply.github.com>
2025-07-02 00:54:03 +0300

    .

--- ComodoTeknoloji/vite.config.js
+++ ComodoTeknoloji/vite.config.js
@@ -2,7 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
+// "base" ayarı, GitHub Pages için repo adınızla aynı olmalı
 export default defineConfig({
-  base: '/',
+  base: '/comodoteknoloji/',
   plugins: [react()],
 })
